0eb1a308e3ee9bf7deac5f7719478131194bdb63
PieterJSchreurs <pieter_j_schreurs@hotmail.com>
2016-11-16 13:58:10 +0100

    Clean

--- DeWaaiBeheer/HomePage.Designer.cs
+++ DeWaaiBeheer/HomePage.Designer.cs
@@ -29,65 +29,32 @@
         private void InitializeComponent()
         {
             this.btnVerder = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnVerder
             // 
-            this.btnVerder.Location = new System.Drawing.Point(310, 104);
+            this.btnVerder.Location = new System.Drawing.Point(310, 109);
             this.btnVerder.Name = "btnVerder";
-            this.btnVerder.Size = new System.Drawing.Size(189, 101);
+            this.btnVerder.Size = new System.Drawing.Size(171, 96);
             this.btnVerder.TabIndex = 1;
             this.btnVerder.Text = "Verder";
             this.btnVerder.UseVisualStyleBackColor = true;
             this.btnVerder.Click += new System.EventHandler(this.btnVerder_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(331, 249);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(61, 40);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
-            this.panel1.TabIndex = 3;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(479, 246);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // HomePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(848, 448);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnVerder);
             this.Name = "HomePage";
             this.Text = "Homepage";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button btnVerder;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
