4f24d31de96b6a0ff8ac805d79121d1240bd7fdb
EmreAtasoy <emreeatasoy@hotmail.com>
2016-11-17 11:00:25 +0100

    Inloggen

---
+++ DeWaaiBeheer/DatabaseMethods.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace DeWaaiBeheer
+{
+    
+    public class DatabaseMethods
+    {
+        private EntityFramework ef = new EntityFramework();
+
+        /// <summary>
+        /// Gets all users of database
+        /// </summary>
+        /// <returns>Returns a list of all users</returns>
+        public ObservableCollection<User> getUsers()
+        {
+            return new ObservableCollection<User>(ef.Users);
+        }
+    }
+}
