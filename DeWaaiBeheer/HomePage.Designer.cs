643d549a183a4693fbf20bde3c5297902304664d
EmreAtasoy <emreeatasoy@hotmail.com>
2016-11-17 09:42:43 +0100

    Label

--- DeWaaiBeheer/HomePage.Designer.cs
+++ DeWaaiBeheer/HomePage.Designer.cs
@@ -35,6 +35,7 @@
             this.btnUsers = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.cmbName = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.tlpMain.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -49,6 +50,7 @@
             this.tlpMain.Controls.Add(this.btnInscriptions, 1, 1);
             this.tlpMain.Controls.Add(this.btnUsers, 0, 1);
             this.tlpMain.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tlpMain.Controls.Add(this.label1, 1, 0);
             this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tlpMain.Location = new System.Drawing.Point(0, 0);
             this.tlpMain.Name = "tlpMain";
@@ -130,6 +132,15 @@
             this.cmbName.TabIndex = 5;
             this.cmbName.SelectedIndexChanged += new System.EventHandler(this.cmbName_SelectedIndexChanged);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(427, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -141,6 +152,7 @@
             this.Text = "Homepage";
             this.Load += new System.EventHandler(this.frnHome_Load);
             this.tlpMain.ResumeLayout(false);
+            this.tlpMain.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -155,6 +167,7 @@
         private System.Windows.Forms.Button btnInscriptions;
         public System.Windows.Forms.ComboBox cmbName;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
