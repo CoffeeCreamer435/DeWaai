d7648063ea882c10ca71ae6c67d73bbec44a9a4c
PieterJSchreurs <pieter_j_schreurs@hotmail.com>
2016-11-17 10:58:10 +0100

    Cursusform

---
+++ DeWaaiBeheer/CursussenForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeWaaiBeheer
+{
+    public partial class CursussenForm : Form
+    {
+        public CursussenForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
